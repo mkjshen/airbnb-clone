502f55a3d606e1dbe5ed1f201cb2056e4c3217a9
mkjshen <118473721+mkjshen@users.noreply.github.com>
2023-06-16 17:21:14 +0800

    Register page not opening

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,31 @@
-import Navbar from './components/navbar/Navbar'
-import './globals.css'
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/Modal";
 
-import { Nunito } from 'next/font/google'
+import { Nunito } from "next/font/google";
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'AirBNB',
-  description: 'AirBNB Clone App',
-}
+  title: "AirBNB",
+  description: "AirBNB Clone App",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <RegisterModal />
+          <Navbar />  
+        </ClientOnly>
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
