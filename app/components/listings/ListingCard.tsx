4bf6af5e53177df4acd0d1d901dd7feb0e38a752
mkjshen <118473721+mkjshen@users.noreply.github.com>
2023-07-04 15:56:10 +0800

    completed half of the individual listing pages

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -6,14 +6,14 @@ import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
 
 import useCountries from "@/app/hooks/useCountries";
-import { SafeUser } from "@/app/types";
+import { SafeUser, SafeListing } from "@/app/types";
 
 import HeartButton from "../HeartButton";
 import Button from "../Button";
-import { Listing, Reservation } from "@prisma/client";
+import { Reservation } from "@prisma/client";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
